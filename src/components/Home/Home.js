2bc2ea606036ccb57d73f7f00bde37b06e34bafb
Huynh Thi Khanh Linh <huynhthikhanhlinh2105@gmail.com>
2021-11-21 16:29:02 +0700

    implement home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,8 +3,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      This is Home
+    <div className="home-container">
+      <div className="home-title">coketriks</div>
+      <div className="home-sub">Let's <strong>coke</strong> with our <strong>triks!</strong></div>
     </div>
   )
 }
